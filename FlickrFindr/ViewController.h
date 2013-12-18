9dd0037a2419995751bb519830f38c659e100486
Tracey Eubanks <traceyeubanks@yahoo.com>
2013-12-18 08:55:42 -0700

    Initial Commit

---
+++ FlickrFindr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlickrFindr
+//
+//  Created by Tracey Eubanks on 12/18/13.
+//  Copyright (c) 2013 Tracey Eubanks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FlickrFindrTests/FlickrFindrTests.m
@@ -0,0 +1,34 @@
+//
+//  FlickrFindrTests.m
+//  FlickrFindrTests
+//
+//  Created by Tracey Eubanks on 12/18/13.
+//  Copyright (c) 2013 Tracey Eubanks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FlickrFindrTests : XCTestCase
+
+@end
+
+@implementation FlickrFindrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
